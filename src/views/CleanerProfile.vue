24014f108b36dff536a490a01d2f6dc8b94dffe9
riskezwn <enriquegarciagasc@gmail.com>
2022-05-27 17:50:56 +0200

    Add modal and functions for checkout cleaner

--- src/views/CleanerProfile.vue
+++ src/views/CleanerProfile.vue
@@ -2,14 +2,20 @@
 import CleanerData from "@/components/cleaner_profile/CleanerData.vue";
 import CleanerDescription from "@/components/cleaner_profile/CleanerDescription.vue";
 import CleanerMap from "@/components/cleaner_profile/CleanerMap.vue";
-import { getDocs, collection } from "firebase/firestore";
+import { getDoc, doc } from "firebase/firestore";
 import { db } from "../../firebase/config";
 import { useRoute } from "vue-router";
 import { onMounted, reactive } from "vue";
+import { useUiStore } from "@/stores/ui";
+import CheckoutModal from "../components/cleaner_profile/CheckoutModal.vue";
+
+const { checkout } = useUiStore();
+
 const route = useRoute();
 const id = route.params.id;
 
 const state = reactive({
+  id: id,
   name: "",
   description: "",
   works: "",
@@ -24,20 +30,17 @@ const state = reactive({
 });
 
 const getData = async () => {
-  const collRef = collection(db, "cleaners");
-  const querySnapshot = await getDocs(collRef);
-  querySnapshot.forEach((doc) => {
-    if (doc.id === id) {
-      state.img = doc.data().image;
-      state.name = doc.data().name;
-      state.city = doc.data().city;
-      state.cp = doc.data().cp;
-      state.price = doc.data().price;
-      state.description = doc.data().description;
-      state.works = doc.data().works;
-      state.rating = doc.data().rating;
-    }
-  });
+  const docRef = doc(db, "cleaners", id);
+  const docSnap = await getDoc(docRef);
+  const data = docSnap.data();
+  state.img = data.image;
+  state.name = data.name;
+  state.city = data.city;
+  state.cp = data.cp;
+  state.price = data.price;
+  state.description = data.description;
+  state.works = data.works;
+  state.rating = data.rating;
 };
 onMounted(() => {
   getData();
@@ -46,6 +49,13 @@ onMounted(() => {
 
 <template>
   <main>
+    <CheckoutModal
+      v-if="checkout.isOpen"
+      :img="state.img"
+      :name="state.name"
+      :price="state.price"
+      :id="state.id"
+    />
     <section class="cleaner-profile">
       <CleanerData
         :img="state.img"
@@ -64,6 +74,9 @@ onMounted(() => {
   </main>
 </template>
 <style lang="scss" scoped>
+main {
+  overflow-y: hidden;
+}
 .cleaner-profile {
   width: 90%;
   margin: 5em auto;
